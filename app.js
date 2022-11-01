84ba59678b8d844d7580f4b4ff300c35fb42a47f
akshat2109 <54941008+akshat2109@users.noreply.github.com>
2022-11-01 21:28:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+//import { ethers } from "ethers";
+
+
+var contractAbi = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "Added",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_productId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "info",
+				"type": "string"
+			}
+		],
+		"name": "addState",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_a",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_b",
+				"type": "string"
+			}
+		],
+		"name": "concat",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_text",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_date",
+				"type": "string"
+			}
+		],
+		"name": "newItem",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_productId",
+				"type": "uint256"
+			}
+		],
+		"name": "searchProduct",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+var contractAddress ='0x4fde5E887Ac5c64dAd23101e4D3591f4855c6780';
+
+
+  /*  let provider;
+
+    if(window.ethereum !== undefined){
+      provider = new ethers.providers.Web3Provider(window.ethereum)
+      await provider.send("eth_requestAccounts", []); 
+      const signer = provider.getSigner()
+      const address = await signer.getAddress()
+	  console.log(address)
+	  //const contract = new ethers.Contract(contract_address, ABI, signer);    
+    } else{
+      console.log("Please install metamask")
+    }
+	*/
